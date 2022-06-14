842578712e74c822b41edbe7758510f8a63dabe7
Lee Sang Won <tkddnjs206@gmail.com>
2022-06-14 20:33:18 +0900

    new

--- src/main/java/com/mysite/sbb/user/UserController.java
+++ src/main/java/com/mysite/sbb/user/UserController.java
@@ -25,6 +25,8 @@ public class UserController {
 
     @PostMapping("/signup")
     public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+        //@Valid 애너테이션을 적용하면 CreateForm의 @NotEmpty, @Size 등으로 설정한 검증 기능이 동작한다.
+        //BindingResult 매개변수는 @Valid 애너테이션으로 인해 검증이 수행된 결과를 의미하는 객체
         if (bindingResult.hasErrors()) {
             return "signup_form";
         }
@@ -52,7 +54,7 @@ public class UserController {
         return "redirect:/";
     }
 
-    @GetMapping("/login")
+    @GetMapping("/login") //post방식을 통한 로그인은 spring security가 처리해준다~
     public String login() {
         return "login_form";
     }
